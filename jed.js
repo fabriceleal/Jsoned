17b0c4fa4bc0e0daa7751323f44c91a6a8a41e77
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-04-09 12:29:53 -0700

    reads command line args ...

--- jed.js
+++ jed.js
@@ -1,16 +1,23 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
-# Usage
+// Using /usr/sbin/node throws a weird error: "axconfig: port 1 is not active" and "axconfig: port 2 is not active"
+
+// Get command line args
+process.argv.forEach(function (val, index, arr){
+	process.stdout.write(index + ': ' + val + '\n');
+});
 
 
-# Read input (json) from stdin
+// Read input (json) from stdin
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function(chunk){
+	
+	
 	process.stdout.write('data = ' + chunk);
 });
 
-# Write result to stdout
+
 
 
