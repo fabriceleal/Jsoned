0f5c61dede59d76f6b64b1a414cd8d22512d031d
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-04-16 05:44:34 -0700

    removed examples, moved to a diferent repository

--- examples/repos_time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-curl "https://api.github.com/users/fabriceleal/repos" | ../jed.js -s "'_.fork == false'" | ../jed.js -m "
-	' [_.name, _.created_at.slice(0, 7), _.pushed_at.slice(0, 7), _.updated_at.slice(0, 7)] '"
